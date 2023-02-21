176ca7035f8d94a205f6b1eba214df1f91469d3b
zhisheng <zhishengtrieu@gmail.com>
2023-02-21 08:58:38 -0500

    debut de tests pour la prediction

--- src/ai/test.py
+++ src/ai/test.py
@@ -1,7 +1,6 @@
-import tensorflow as tf
 import cv2
 import matplotlib.pyplot as plt
-from tensorflow.keras import datasets, layers, models
+from tensorflow.keras import models
 
 # on charge le modele
 model = models.load_model("src/ai/modeles/modele_immo.h5")
@@ -11,4 +10,8 @@ image_to_predict = cv2.imread("dataset/1_kitchen.jpg", cv2.IMREAD_COLOR)
 plt.imshow(cv2.cvtColor(image_to_predict, cv2.COLOR_BGR2RGB))
 
 # on redimensionne l'image
-image_to_predict = cv2.resize(image_to_predict, (200, 200))
+image_to_predict = cv2.resize(image_to_predict, (64, 64))
+
+# on teste le modele
+predictions = model.predict(image_to_predict.reshape(1, 64, 64, 3))
+print(predictions)
